48d6aec4e942d8727a24a8cb913b4ec162ef80ae
kazuki <pharcydetip@gmail.com>
2020-10-24 23:23:54 +0900

    add nonce method

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,4 +1,6 @@
 package example
 
-case class  CoincheckApi(apiKey: String, apiSecret: String) {}
+case class  CoincheckApi(apiKey: String, apiSecret: String) {
+  private def nonce = (System.currentTimeMillis() / 1000).toString
+}
 
