efe8977881cc4afd16875e16f6d6cd25bd63732f
kazuki <pharcydetip@gmail.com>
2020-10-27 02:22:12 +0900

    impl Transaction

---
+++ domain/src/main/scala/exchange/Transaction.scala
@@ -0,0 +1,29 @@
+package domain.exchange
+
+import domain.currency.Currency
+import domain.exchange.Transaction.{TraCreatedAt, TraId, TraRate, TraSide}
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.numeric.Positive
+import eu.timepit.refined.types.string.NonEmptyString
+import io.estatico.newtype.macros.newtype
+
+final case class Transaction(
+  id: TraId,
+  sold: Currency,
+  bought: Currency,
+  side: TraSide,
+  createdAt: TraCreatedAt,
+  rate: TraRate
+)
+
+object Transaction {
+  @newtype case class TraId(value: Long Refined Positive)
+  @newtype case class TraCreatedAt(
+    value: NonEmptyString
+  ) // TODO: validate with iso date regex
+  @newtype case class TraRate(value: Double Refined Positive)
+
+  sealed trait TraSide
+  case object Buy  extends TraSide
+  case object Sell extends TraSide
+}
