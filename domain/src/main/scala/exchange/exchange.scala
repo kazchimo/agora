c8209c07a985af9f1242783f5deaa79fa105fde3
kazuki <pharcydetip@gmail.com>
2020-10-27 03:06:33 +0900

    impl IExchange

--- domain/src/main/scala/exchange/exchange.scala
+++ domain/src/main/scala/exchange/exchange.scala
@@ -1,5 +1,22 @@
 package exchange
 
+import eu.timepit.refined.types.string.NonEmptyString
+import exchange.ExchangeConfig.{CCApiKey, CCSecretKey}
+import io.estatico.newtype.macros.newtype
+import zio.ZIO
 
-trait IExchange {
+trait IExchange[T <: IExchange[_]] {
+  type Conf = ExchangeConfig[T]
+
+  def transactions: ZIO[Conf, String, String]
+}
+
+trait ExchangeConfig[T <: IExchange[_]] {
+  val apiKey: CCApiKey
+  val secretKey: CCSecretKey
+}
+
+object ExchangeConfig {
+  @newtype final case class CCApiKey(value: NonEmptyString)
+  @newtype final case class CCSecretKey(value: NonEmptyString)
 }
