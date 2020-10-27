f15f75c45c55b017af6faab55171f97585ae880d
kazuki <pharcydetip@gmail.com>
2020-10-28 00:24:07 +0900

    fix imports

--- infra/src/main/scala/exchange/CoinCheckExchange.scala
+++ infra/src/main/scala/exchange/CoinCheckExchange.scala
@@ -7,7 +7,7 @@ import eu.timepit.refined.auto._
 import eu.timepit.refined.collection.NonEmpty
 import eu.timepit.refined.refineV
 import eu.timepit.refined.types.string.NonEmptyString
-import exchange.CoinCheckExchangeConfig.{CCEApiKey, CCESecretKey}
+import CoinCheckExchangeConfig.{CCEApiKey, CCESecretKey}
 import io.estatico.newtype.macros.newtype
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
