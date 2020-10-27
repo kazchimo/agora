29125acb4eaea91ec9c8356077541c16777eaa43
kazuki <pharcydetip@gmail.com>
2020-10-28 01:42:51 +0900

    install zio sttp

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,15 +2,18 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    lazy val zio = "1.0.3"
+    lazy val zio  = "1.0.3"
+    lazy val sttp = "3.0.0-RC6"
   }
 
-  lazy val scalaTest  = "org.scalatest"                 %% "scalatest"     % "3.2.2"
-  lazy val sttp       = "com.softwaremill.sttp.client3" %% "core"          % "3.0.0-RC6"
-  lazy val zio        = "dev.zio"                       %% "zio"           % Versions.zio
-  lazy val ziostreams = "dev.zio"                       %% "zio-streams"   % Versions.zio
-  lazy val zioMacros  = "dev.zio"                       %% "zio-macros"    % Versions.zio
-  lazy val codec      = "commons-codec"                  % "commons-codec" % "1.15"
-  lazy val newtype    = "io.estatico"                   %% "newtype"       % "0.4.4"
-  lazy val refined    = "eu.timepit"                    %% "refined"       % "0.9.17"
+  lazy val scalaTest = "org.scalatest"                 %% "scalatest" % "3.2.2"
+  lazy val sttp      = "com.softwaremill.sttp.client3" %% "core"      % Versions.sttp
+  lazy val sttpZio   =
+    "com.softwaremill.sttp.client3" %% "async-http-client-backend-zio" % Versions.sttp
+  lazy val zio        = "dev.zio"      %% "zio"           % Versions.zio
+  lazy val ziostreams = "dev.zio"      %% "zio-streams"   % Versions.zio
+  lazy val zioMacros  = "dev.zio"      %% "zio-macros"    % Versions.zio
+  lazy val codec      = "commons-codec" % "commons-codec" % "1.15"
+  lazy val newtype    = "io.estatico"  %% "newtype"       % "0.4.4"
+  lazy val refined    = "eu.timepit"   %% "refined"       % "0.9.17"
 }
