1bfac53c846ffe829d9a0b5bac0b3b9e1ae50464
kazuki <pharcydetip@gmail.com>
2020-10-28 21:57:17 +0900

    impl vo factories

--- domain/src/main/scala/exchange/Transaction.scala
+++ domain/src/main/scala/exchange/Transaction.scala
@@ -3,9 +3,12 @@ package exchange
 import currency.Currency
 import exchange.Transaction.{TraCreatedAt, TraId, TraRate, TraSide}
 import eu.timepit.refined.api.Refined
+import eu.timepit.refined.collection.NonEmpty
 import eu.timepit.refined.numeric.Positive
+import eu.timepit.refined.refineV
 import eu.timepit.refined.types.string.NonEmptyString
 import io.estatico.newtype.macros.newtype
+import zio.{IO, ZIO}
 
 final case class Transaction(
   id: TraId,
@@ -18,10 +21,24 @@ final case class Transaction(
 
 object Transaction {
   @newtype case class TraId(value: Long Refined Positive)
+  object TraId {
+    def apply(v: Long): IO[String, TraId] =
+      ZIO.fromEither(refineV[Positive](v)).map(TraId(_))
+  }
+
   @newtype case class TraCreatedAt(
     value: NonEmptyString
   ) // TODO: validate with iso date regex
+  object TraCreatedAt {
+    def apply(v: String): IO[String, TraCreatedAt] =
+      ZIO.fromEither(refineV[NonEmpty](v)).map(TraCreatedAt(_))
+  }
+
   @newtype case class TraRate(value: Double Refined Positive)
+  object TraRate {
+    def apply(v: Double): IO[String, TraRate] =
+      ZIO.fromEither(refineV[Positive](v)).map(TraRate(_))
+  }
 
   sealed trait TraSide
   case object Buy  extends TraSide
