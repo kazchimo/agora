f0fab0fc2f890a1e1f9a794ac60d6a6e035a19d5
kazuki <pharcydetip@gmail.com>
2020-10-29 02:29:20 +0900

    add refined lib

---
+++ lib/src/main/scala/lib/refined/package.scala
@@ -0,0 +1,15 @@
+package lib
+
+import eu.timepit.refined.api.{Refined, Validate}
+import eu.timepit.refined.refineV
+import zio.{IO, Task, ZIO}
+
+package object refined {
+  def refineVZ[P, T](t: T)(implicit
+    v: Validate[T, P]
+  ): IO[String, Refined[T, P]] = ZIO.fromEither(refineV[P](t))
+
+  def refineVZE[P, T](t: T)(implicit
+    v: Validate[T, P]
+  ): Task[Refined[T, P]] = ZIO.effect(refineV[P].unsafeFrom(t))
+}
