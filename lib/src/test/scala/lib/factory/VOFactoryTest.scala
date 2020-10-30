80d7af965f4fdae6bd389bb9e01a6a2cf9bf34f7
kazuki <pharcydetip@gmail.com>
2020-10-31 02:15:12 +0900

    add VOFactory test

---
+++ lib/src/test/scala/lib/factory/VOFactoryTest.scala
@@ -0,0 +1,54 @@
+package lib.factory
+
+import eu.timepit.refined.collection.NonEmpty
+import eu.timepit.refined.types.string.NonEmptyString
+import io.estatico.newtype.macros.newtype
+import zio.test.Assertion._
+import zio.test._
+
+object VOFactoryTest extends DefaultRunnableSpec {
+  @newtype case class V(v: NonEmptyString)
+  object V extends VOFactory[String, NonEmpty] {
+    override type VO = V
+  }
+
+  val nonEmptyStringGen = for {
+    s <- Gen.anyString
+    c <- Gen.anyChar
+  } yield c.toString + s
+
+  val applyTest = suite("apply")(
+    testM("invalid value fails with Throwable")(
+      assertM(V("").run)(fails(isSubtype[IllegalArgumentException](anything)))
+    ),
+    testM("valid value creates a object") {
+      checkM(nonEmptyStringGen) { s =>
+        assertM(V(s))(equalTo(V(NonEmptyString.unsafeFrom(s))))
+      }
+    }
+  )
+
+  val applySTest = suite("applyS")(
+    testM("invalid value fails with String")(
+      assertM(V.applyS("").run)(fails(isSubtype[String](anything)))
+    ),
+    testM("valid value creates a object") {
+      checkM(nonEmptyStringGen) { s =>
+        assertM(V.applyS(s))(equalTo(V(NonEmptyString.unsafeFrom(s))))
+      }
+    }
+  )
+
+  val unsafeFromTest = suite("unsafeFrom")(
+    test("invalid value throws Exception") {
+      assert(V.unsafeFrom(""))(throwsA[IllegalArgumentException])
+    },
+    testM("valid value creates a object") {
+      check(nonEmptyStringGen) { s =>
+        assert(V.unsafeFrom(s))(equalTo(V(NonEmptyString.unsafeFrom(s))))
+      }
+    }
+  )
+
+  override def spec = suite("VOFactory")(applyTest, applySTest)
+}
