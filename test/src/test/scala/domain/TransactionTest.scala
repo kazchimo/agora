e10b5eb04463926ef5194c4b8c0bffa8cea66eec
kazuki <pharcydetip@gmail.com>
2020-10-31 03:21:47 +0900

    add TraSide test

---
+++ test/src/test/scala/domain/TransactionTest.scala
@@ -0,0 +1,20 @@
+package domain
+
+import domain.exchange.Transaction.{Buy, Sell}
+import zio.test._
+import zio.test.Assertion._
+
+object TransactionTest extends DefaultRunnableSpec {
+  override def spec = suite("Transaction")(traSideTest)
+
+  val traSideTest = suite("TraSide")(
+    suite("isBuy")(
+      test("Buy#isBuy returns true")(assert(Buy.isBuy)(equalTo(true))),
+      test("Sell#isBuy returns false")(assert(Sell.isBuy)(equalTo(false)))
+    ),
+    suite("isSell")(
+      test("Buy#isSell returns false")(assert(Buy.isSell)(equalTo(false))),
+      test("Sell#isSell returns true")(assert(Sell.isSell)(equalTo(true)))
+    )
+  )
+}
