912b0392a3c014d01d0ce83dc71bb3a1669bb11b
kazuki <pharcydetip@gmail.com>
2020-10-31 14:59:23 +0900

    add orders interface

--- domain/src/main/scala/domain/exchange/coincheck/package.scala
+++ domain/src/main/scala/domain/exchange/coincheck/package.scala
@@ -1,6 +1,6 @@
 package domain.exchange
 
-import zio.{Has, IO, ZIO}
+import zio.{Has, IO, Task, ZIO}
 
 package object coincheck {
   type CoincheckExchange = Has[CoincheckExchange.Service]
@@ -8,10 +8,14 @@ package object coincheck {
   object CoincheckExchange {
     trait Service {
       def transactions: IO[Throwable, Seq[CCTransaction]]
+      def orders: Task[Unit]
     }
 
     def transactions: ZIO[CoincheckExchange, Throwable, Seq[CCTransaction]] =
       ZIO.accessM(_.get.transactions)
+
+    def orders: ZIO[CoincheckExchange, Throwable, Unit] =
+      ZIO.accessM(_.get.orders)
   }
 
 }
