ebac498d14528b3fefb354c71ac4ea0452ca76bc
kazuki <pharcydetip@gmail.com>
2020-10-31 11:21:26 +0900

    impl orders

---
+++ domain/src/main/scala/domain/exchange/Order.scala
@@ -0,0 +1,13 @@
+package domain.exchange
+
+sealed trait Order
+
+final case class Buy() extends Order
+
+final case class Sell() extends Order
+
+final case class MarketBuy() extends Order
+
+final case class MarketSell() extends Order
+
+final case class MarketStopSell() extends Order
