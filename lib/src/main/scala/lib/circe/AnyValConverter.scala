5fbd1777bdb0707d4574b054d2ab4216fe30cbf8
kazuki <pharcydetip@gmail.com>
2020-10-31 15:42:36 +0900

    add AnyValConverter

---
+++ lib/src/main/scala/lib/circe/AnyValConverter.scala
@@ -0,0 +1,24 @@
+package lib.circe
+
+import io.circe.{Decoder, Encoder}
+
+trait AnyValConverter {
+  import shapeless._
+
+  implicit final def decodeAnyVal[T, U: Decoder](implicit
+    ev: T <:< AnyVal,
+    unwrapped: Unwrapped.Aux[T, U],
+    decoder: Decoder[U]
+  ): Decoder[T] =
+    Decoder.instance[T] { cursor =>
+      decoder(cursor).map(value => unwrapped.wrap(value))
+    }
+
+  implicit final def encodeAnyVal[T, U](implicit
+    ev: T <:< AnyVal,
+    unwrapped: Unwrapped.Aux[T, U],
+    encoder: Encoder[U]
+  ): Encoder[T] =
+    Encoder.instance[T](value => encoder(unwrapped.unwrap(value)))
+
+}
