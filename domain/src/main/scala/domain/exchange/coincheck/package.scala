89ec897f613bfd1bb68745330943eeee002c8f2e
kazuki <pharcydetip@gmail.com>
2020-11-01 16:23:31 +0900

    impl coincheck module

--- domain/src/main/scala/domain/exchange/coincheck/package.scala
+++ domain/src/main/scala/domain/exchange/coincheck/package.scala
@@ -1,11 +1,12 @@
 package domain.exchange
 
 import sttp.client3.asynchttpclient.zio.SttpClient
-import zio.stream.ZStream
+import zio.stream.{Stream, ZStream}
 import zio.{Has, IO, RIO, ZIO}
 
 package object coincheck {
   type CoincheckExchange = Has[CoincheckExchange.Service]
+  type CoincheckEnv      = SttpClient with CoincheckExchange
 
   object CoincheckExchange {
     trait Service {
@@ -15,13 +16,15 @@ package object coincheck {
         : ZIO[SttpClient, Throwable, ZStream[Any, Nothing, String]]
     }
 
-    def transactions: ZIO[CoincheckExchange, Throwable, Seq[CCTransaction]] =
-      ZIO.accessM(_.get.transactions)
+    def transactions: ZIO[CoincheckEnv, Throwable, Seq[CCTransaction]] =
+      ZIO.accessM(_.get[CoincheckExchange.Service].transactions)
 
-    def orders(
-      order: CCOrder
-    ): ZIO[SttpClient with CoincheckExchange, Throwable, Unit] =
+    def orders(order: CCOrder): ZIO[CoincheckEnv, Throwable, Unit] =
       ZIO.accessM(_.get[CoincheckExchange.Service].orders(order))
   }
+
+  def publicTransactions
+    : ZIO[CoincheckEnv, Throwable, Stream[Nothing, String]] =
+    ZIO.accessM(_.get[CoincheckExchange.Service].publicTransactions)
 
 }
