73e4676f35227850a45bb549a7e9262e010da27c
kazuki <pharcydetip@gmail.com>
2020-11-03 18:14:18 +0900

    make object trait

--- test/src/test/scala/infra/exchange/coincheck/impl/CoinCheckExchangeImplTest.scala
+++ test/src/test/scala/infra/exchange/coincheck/impl/CoinCheckExchangeImplTest.scala
@@ -1,8 +1,17 @@
 package infra.exchange.coincheck.impl
 
-import infra.exchange.coincheck.impl.TransactionsTest.transactionsSuite
+import domain.conf.{CCEAccessKey, CCESecretKey}
 import zio.test.{suite, DefaultRunnableSpec}
 
-object CoinCheckExchangeImplTest extends DefaultRunnableSpec {
+object CoinCheckExchangeImplTest
+    extends DefaultRunnableSpec
+    with TransactionsTest {
+  val exchange = CoinCheckExchangeImpl(
+    CCEAccessKey.unsafeFrom("hoge"),
+    CCESecretKey.unsafeFrom("hoge")
+  )
+  val failJson =
+    "{\"success\":false,\"error\":\"Nonce must be incremented\"}"
+
   override def spec = suite("CoinCheckExchangeImpl")(transactionsSuite)
 }
--- test/src/test/scala/infra/exchange/coincheck/impl/TransactionsTest.scala
+++ test/src/test/scala/infra/exchange/coincheck/impl/TransactionsTest.scala
@@ -1,18 +1,12 @@
 package infra.exchange.coincheck.impl
 
-import domain.conf.{CCEAccessKey, CCESecretKey}
 import infra.InfraError
 import sttp.client3.asynchttpclient.zio.AsyncHttpClientZioBackend
 import sttp.client3.asynchttpclient.zio.stubbing._
 import zio.test.Assertion._
 import zio.test._
 
-object TransactionsTest {
-  val exchange = CoinCheckExchangeImpl(
-    CCEAccessKey.unsafeFrom("hoge"),
-    CCESecretKey.unsafeFrom("hoge")
-  )
-
+trait TransactionsTest { self: CoinCheckExchangeImplTest.type =>
   val transactionsSuite =
     suite("#transactions")(
       testM("fails with Internal Server Error") {
@@ -24,8 +18,6 @@ object TransactionsTest {
         )(fails(isSubtype[InfraError](anything)))
       },
       testM("fails with failed response") {
-        val failJson   =
-          "{\"success\":false,\"error\":\"Nonce must be incremented\"}"
         val testEffect =
           whenAnyRequest.thenRespond(failJson) *>
             exchange.transactions
