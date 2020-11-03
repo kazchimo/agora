9cffa40257d8ee573180418d0547deccc774d4a0
kazuki <pharcydetip@gmail.com>
2020-11-03 13:19:36 +0900

    add negative gens

--- test/src/test/scala/helpers/gen/std/StdGen.scala
+++ test/src/test/scala/helpers/gen/std/StdGen.scala
@@ -4,8 +4,15 @@ import zio.random.Random
 import zio.test.Gen
 
 object StdGen {
-  val positiveLongGen: Gen[Random, Long]     =
+  val positiveLongGen: Gen[Random, Long] =
     Gen.anyLong.map(a => if (a < 0) -a else a)
+
   val positiveDoubleGen: Gen[Random, Double] =
     Gen.anyDouble.map(a => if (a < 0) -a else a)
+
+  val negativeLongGen: Gen[Random, Long] =
+    Gen.anyLong.map(a => if (a > 0) -a else a)
+
+  val negativeDoubleGen: Gen[Random, Long] =
+    Gen.anyLong.map(a => if (a > 0) -a else a)
 }
