114338df5f1ca2feada9f74c1a045c15611c6935
kazuki <pharcydetip@gmail.com>
2020-11-04 20:41:19 +0900

    define bf price

--- domain/src/main/scala/domain/exchange/bitflyer/FBOrder.scala
+++ domain/src/main/scala/domain/exchange/bitflyer/FBOrder.scala
@@ -1,5 +1,11 @@
 package domain.exchange.bitflyer
 
+import domain.exchange.bitflyer.BFChildOrder.BFOrderPrice
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.numeric.Positive
+import io.estatico.newtype.macros.newtype
+import lib.factory.VOFactory
+
 sealed abstract class BFChildOrderType(val v: String)
 case object BFLimitOrderType  extends BFChildOrderType("LIMIT")
 case object BFMarketOrderType extends BFChildOrderType("MARKET")
@@ -8,5 +14,10 @@ sealed abstract class BFOrderSide(val v: String)
 case object BFBuy  extends BFChildOrderType("BUY")
 case object BFSell extends BFChildOrderType("SELL")
 
-final case class BFChildOrder(tpe: BFChildOrderType, side: BFOrderSide)
-bject BFChildOrder {}
+final case class BFChildOrder(tpe: BFChildOrderType, side: BFOrderSide, price: BFOrderPrice)
+object BFChildOrder {
+  @newtype case class BFOrderPrice(v: Long Refined Positive)
+  object BFOrderPrice extends VOFactory[Long, Positive] {
+    override type VO = BFOrderPrice
+  }
+}
