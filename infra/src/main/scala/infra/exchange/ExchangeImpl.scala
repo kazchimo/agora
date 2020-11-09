e8e9f04edecdb707885b410e076822022667df0d
kazuki <pharcydetip@gmail.com>
2020-11-10 00:18:19 +0900

    impl layer

--- infra/src/main/scala/infra/exchange/ExchangeImpl.scala
+++ infra/src/main/scala/infra/exchange/ExchangeImpl.scala
@@ -1,7 +1,9 @@
 package infra.exchange
 
 import domain.conf.Conf
+import domain.exchange.bitflyer.BitflyerExchange
 import domain.exchange.coincheck.CoincheckExchange
+import infra.exchange.bitflyer.impl.BitflyerExchangeImpl
 import infra.exchange.coincheck.impl.CoinCheckExchangeImpl
 import zio.ZLayer
 
@@ -9,5 +11,11 @@ object ExchangeImpl {
   val coinCheckExchange: ZLayer[Conf, Throwable, CoincheckExchange] =
     ZLayer.fromFunctionM(conf =>
       conf.get.CCAccessKey.zipWith(conf.get.CCSecretKey)(CoinCheckExchangeImpl)
+    )
+
+  val bitflyerExchange: ZLayer[Conf, Throwable, BitflyerExchange] =
+    ZLayer.fromFunctionM(conf =>
+      conf.get.BFAccessKey
+        .zipWith(conf.get.BFSecretKey)(BitflyerExchangeImpl)
     )
 }
--- infra/src/main/scala/infra/exchange/bitflyer/impl/BitflyerExchangeImpl.scala
+++ infra/src/main/scala/infra/exchange/bitflyer/impl/BitflyerExchangeImpl.scala
@@ -4,8 +4,8 @@ import domain.conf.{BFAccessKey, BFSecretKey}
 import domain.exchange.bitflyer.BitflyerExchange
 
 final case class BitflyerExchangeImpl(
-  secretKey: BFSecretKey,
-  accessKey: BFAccessKey
+  accessKey: BFAccessKey,
+  secretKey: BFSecretKey
 ) extends BitflyerExchange.Service
     with AuthStrategy
     with ChildOrder
