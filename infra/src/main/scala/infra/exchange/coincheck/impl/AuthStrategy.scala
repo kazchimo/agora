fd10341a873bb2e8fbbf8eb4716e191fda0d7f75
kazuki <pharcydetip@gmail.com>
2020-11-09 22:50:49 +0900

    refact

--- infra/src/main/scala/infra/exchange/coincheck/impl/AuthStrategy.scala
+++ infra/src/main/scala/infra/exchange/coincheck/impl/AuthStrategy.scala
@@ -3,13 +3,14 @@ package infra.exchange.coincheck.impl
 import lib.cripto.HmacSha256Encode.hmacSHA256Encode
 import zio.{Task, ZIO}
 
-private[exchange] trait AuthStrategy { self: CoinCheckExchangeImpl =>
+private[coincheck] trait AuthStrategy { self: CoinCheckExchangeImpl =>
   type Header = Map[String, String]
 
   final protected def headers(url: String, body: String = ""): Task[Header] =
     for {
       nonce <- ZIO.effectTotal(createNonce)
-      sig   <- createSig(secretKey.value.value, url, nonce, body)
+      sig   <- hmacSHA256Encode(secretKey.value.value, nonce + url + body)
+
     } yield Map(
       "ACCESS-KEY"       -> accessKey.value.value,
       "ACCESS-NONCE"     -> nonce,
@@ -17,13 +18,4 @@ private[exchange] trait AuthStrategy { self: CoinCheckExchangeImpl =>
     )
 
   private def createNonce = (System.currentTimeMillis() / 1000).toString
-
-  private def createSig(
-    secretKey: String,
-    url: String,
-    nonce: String,
-    body: String
-  ) =
-    hmacSHA256Encode(secretKey, nonce + url + body)
-
 }
