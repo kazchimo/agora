02a6462f02a5f977650c84e23ccf891ba20e2799
kazuki <pharcydetip@gmail.com>
2020-11-09 22:57:45 +0900

    impl AuthStrategy

---
+++ infra/src/main/scala/infra/exchange/bitflyer/impl/AuthStrategy.scala
@@ -0,0 +1,19 @@
+package infra.exchange.bitflyer.impl
+
+import lib.cripto.HmacSha256Encode.hmacSHA256Encode
+import zio.{Task, ZIO}
+
+private[bitflyer] trait AuthStrategy { self: BitflyerExchangeImpl =>
+  type Header = Map[String, String]
+
+  def headers(method: String, path: String, body: String): Task[Header] = for {
+    ts   <- ZIO.effectTotal(timestamp)
+    sign <- hmacSHA256Encode(secretKey.value.value, ts + method + path + body)
+  } yield Map(
+    "ACCESS-KEY"       -> accessKey.value.value,
+    "ACCESS-TIMESTAMP" -> ts,
+    "ACCESS-SIGN"      -> sign
+  )
+
+  private def timestamp = (System.currentTimeMillis() / 1000).toString
+}
