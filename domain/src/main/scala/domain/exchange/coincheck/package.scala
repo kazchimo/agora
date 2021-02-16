262d2c4aa43504207c33ba8b0bb773a85d3c20a0
kazuki <pharcydetip@gmail.com>
2021-02-16 13:59:00 +0900

    refactor: use zio-loggign instead of putStrLn

--- domain/src/main/scala/domain/exchange/coincheck/package.scala
+++ domain/src/main/scala/domain/exchange/coincheck/package.scala
@@ -2,6 +2,7 @@ package domain.exchange
 
 import sttp.client3.asynchttpclient.zio.SttpClient
 import zio._
+import zio.logging.Logging
 import zio.macros.accessible
 import zio.stream.Stream
 
@@ -13,8 +14,11 @@ package object coincheck {
     trait Service {
       def transactions: RIO[SttpClient, Seq[CCTransaction]]
       def orders(order: CCOrder): RIO[SttpClient with ZEnv, Unit]
-      def publicTransactions
-        : ZIO[SttpClient with ZEnv, Throwable, Stream[Nothing, String]]
+      def publicTransactions: ZIO[
+        SttpClient with ZEnv with Logging,
+        Throwable,
+        Stream[Nothing, String]
+      ]
     }
   }
 }
--- infra/src/main/scala/infra/exchange/coincheck/impl/PublicTransactions.scala
+++ infra/src/main/scala/infra/exchange/coincheck/impl/PublicTransactions.scala
@@ -6,41 +6,42 @@ import sttp.client3._
 import sttp.client3.asynchttpclient.zio.{SttpClient, sendR}
 import sttp.ws.WebSocket
 import zio._
-import zio.console.putStrLn
+import zio.logging.{Logging, log}
 import zio.stream.Stream
 
 private[exchange] trait PublicTransactions { self: CoinCheckExchangeImpl =>
-  private def useWS(que: Queue[String])(ws: WebSocket[RIO[ZEnv, *]]) = {
+  private def useWS(
+    que: Queue[String]
+  )(ws: WebSocket[RIO[ZEnv with Logging, *]]) = {
     val send    =
       ws.sendText("{\"type\":\"subscribe\",\"channel\":\"btc_jpy-trades\"}")
     val receive = for {
       textEither <- ws.receiveTextFrame()
-      _          <- putStrLn(s"received: ${textEither.toString}")
       text       <-
         ZIO
           .fromEither(textEither)
-          .onError(_ => putStrLn("shutdown") *> que.shutdown)
+          .onError(_ => log.error("shutdown") *> que.shutdown)
           .mapError(c =>
             InfraError(
               s"close websocket connection: statusCode=${c.statusCode.toString} reason=${c.reasonText}"
             )
           )
-      _          <- putStrLn(text.toString)
       _          <- que.offer(text.payload)
     } yield ()
 
-    send *> putStrLn("send websocket") *> receive.forever
+    send *> receive.forever
   }
 
-  final override def publicTransactions
-    : ZIO[SttpClient with ZEnv, Throwable, Stream[Nothing, String]] = for {
-    _   <- putStrLn("start public transactions")
+  final override def publicTransactions: ZIO[
+    SttpClient with ZEnv with Logging,
+    Throwable,
+    Stream[Nothing, String]
+  ] = for {
     que <- Queue.unbounded[String]
-    _   <- sendR[Unit, ZEnv](
+    _   <- sendR[Unit, ZEnv with Logging](
              basicRequest
                .get(uri"${Endpoints.websocket}")
                .response(asWebSocketAlways(useWS(que)))
            ).fork
-    _   <- putStrLn("send complete")
   } yield Stream.fromQueueWithShutdown(que).interruptWhen(que.awaitShutdown)
 }
