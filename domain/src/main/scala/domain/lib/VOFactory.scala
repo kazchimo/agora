015e34f123e7d2b051bba177323a91bb83459049
kazuki <pharcydetip@gmail.com>
2021-02-18 01:37:37 +0900

    feat: map error to ClientDomainErrorr

--- domain/src/main/scala/domain/lib/VOFactory.scala
+++ domain/src/main/scala/domain/lib/VOFactory.scala
@@ -1,16 +1,18 @@
 package domain.lib
 
 import eu.timepit.refined.api.{Refined, Validate}
+import lib.error.ClientDomainError
 import lib.refined.refineVZE
-import zio.{IO, Task}
+import zio.IO
 
 trait VOFactory[V, P] {
   type VO
 
   def apply(v: V Refined P): VO
 
-  final def apply(v: V)(implicit V: Validate[V, P]): Task[VO] =
-    refineVZE[P, V](v).map(apply)
+  final def apply(v: V)(implicit V: Validate[V, P]): IO[ClientDomainError, VO] =
+    refineVZE[P, V](v)
+      .bimap(e => ClientDomainError("Failed to create VO", Some(e)), apply)
 
   final def applyS(v: V)(implicit V: Validate[V, P]): IO[String, VO] =
     refineVZE[P, V](v).bimap(_.getMessage, apply)
