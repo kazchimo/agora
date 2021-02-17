f5450257ef5066be50440e123f9de43a557b4488
kazuki <pharcydetip@gmail.com>
2021-02-18 01:38:18 +0900

    fix: use client error instead of internal error

--- domain/src/main/scala/domain/lib/EnumZio.scala
+++ domain/src/main/scala/domain/lib/EnumZio.scala
@@ -1,12 +1,12 @@
 package domain.lib
 import enumeratum._
-import lib.error.InternalDomainError
+import lib.error.ClientDomainError
 import zio.{IO, ZIO}
 
 trait EnumZio[A <: EnumEntry] { self: Enum[A] =>
-  final def withNameZio(entryName: String): IO[InternalDomainError, A] = ZIO
+  final def withNameZio(entryName: String): IO[ClientDomainError, A] = ZIO
     .fromEither(self.withNameEither(entryName)).mapError(e =>
-      InternalDomainError(
+      ClientDomainError(
         s"Failed to create Enum model from String: String=$entryName",
         Some(e)
       )
