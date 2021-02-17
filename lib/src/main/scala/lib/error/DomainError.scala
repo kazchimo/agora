1bee6bff560242970c5d41a7d117900c4ce74754
kazuki <pharcydetip@gmail.com>
2021-02-18 00:59:23 +0900

    fix: rename domain error

--- lib/src/main/scala/lib/error/DomainError.scala
+++ lib/src/main/scala/lib/error/DomainError.scala
@@ -6,12 +6,12 @@ sealed abstract class DomainError(
   override val code: ErrorCode
 ) extends Error(msg, cause, code)
 
-final case class DomainClientError(
+final case class ClientDomainError(
   override val msg: String,
   override val cause: Option[Throwable]
 ) extends DomainError(msg, cause, ClientErr)
 
-final case class DomainInternalError(
+final case class InternalDomainError(
   override val msg: String,
   override val cause: Option[Throwable]
 ) extends DomainError(msg, cause, InternalErr)
