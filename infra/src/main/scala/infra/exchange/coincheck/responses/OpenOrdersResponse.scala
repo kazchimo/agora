b53696881d4b049bc45034bea48190c840bcf746
kazuki <pharcydetip@gmail.com>
2021-02-19 00:55:21 +0900

    feat: add OpenOrdersResponseDecoder

--- infra/src/main/scala/infra/exchange/coincheck/responses/OpenOrdersResponse.scala
+++ infra/src/main/scala/infra/exchange/coincheck/responses/OpenOrdersResponse.scala
@@ -1,6 +1,18 @@
 package infra.exchange.coincheck.responses
 
+import io.circe.Decoder
+import cats.syntax.functor._
+import io.circe.generic.auto._
+
 sealed trait OpenOrdersResponse extends CoincheckResponse
+
+object OpenOrdersResponse {
+  implicit val openOrdersResponseDecoder: Decoder[OpenOrdersResponse] =
+    List[Decoder[OpenOrdersResponse]](
+      Decoder[SuccessOpenOrdersResponse].widen,
+      Decoder[FailedOpenOrdersResponse].widen
+    ).reduceLeft(_ or _)
+}
 
 final case class SuccessOpenOrdersResponse(orders: Seq[OrderContent])
     extends OpenOrdersResponse with SuccessCoincheckResponse
