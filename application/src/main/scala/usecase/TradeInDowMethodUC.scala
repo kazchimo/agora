aa08b9914f922d8560b7026df9bc8cd8d88e9de0
kazuki <pharcydetip@gmail.com>
2021-02-18 23:31:20 +0900

    refactor: use DowMethod strategy

--- application/src/main/scala/usecase/TradeInDowMethodUC.scala
+++ application/src/main/scala/usecase/TradeInDowMethodUC.scala
@@ -1,9 +1,9 @@
 package usecase
 
-import domain.chart.OHLCBar
-import domain.exchange.coincheck.{CCPublicTransaction, CoincheckExchange}
+import domain.exchange.coincheck.CoincheckExchange
+import domain.strategy.DowMethod
+import zio.Ref
 import zio.logging.log
-import zio.{Chunk, Ref}
 
 final case class TradingState(
   onLong: Boolean,
@@ -21,56 +21,33 @@ final case class TradingState(
 }
 
 object TradeInDowMethodUC {
-  private def shouldBuy(bars: Seq[OHLCBar]): Boolean =
-    bars.sortBy(_.high) == bars & bars.sortBy(_.low) == bars
-
-  private def shouldSell(bars: Seq[OHLCBar]): Boolean =
-    bars.sortBy(b => -b.high) == bars & bars.sortBy(b => -b.low) == bars
-
-  private def updateBars[A](barsRef: Ref[Seq[A]], continuous: Int, newBar: A) =
-    barsRef.updateAndGet(old =>
-      if (old.size >= continuous) old.:+(newBar).tail
-      else old.:+(newBar)
-    )
-
   def trade(aggCount: Int, buyContinuous: Int, sellContinuous: Int) = for {
     _                  <- log.info("Buying in Dow method start...")
     transactionsStream <- CoincheckExchange.publicTransactions
-    barsForBuyRef      <- Ref.make(Seq.empty[OHLCBar])
-    barsForSellRef     <- Ref.make(Seq.empty[OHLCBar])
     tradingStateRef    <- Ref.make(TradingState(false, 0, 0))
-    _                  <- transactionsStream
-                            .tap(a => log.trace(a.toString))
-                            .grouped(aggCount).map(OHLCBar.fromTransactions).tap(a =>
-                              log.info(s"High and Low per ${aggCount.toString}: ${a.toString}")
-                            ).foreach { bar =>
-                              for {
-                                barsForBuy   <- updateBars(barsForBuyRef, buyContinuous, bar)
-                                barsForSell  <- updateBars(barsForSellRef, sellContinuous, bar)
-                                tradingState <- tradingStateRef.get
-                                _            <- {
-                                  val buyIf  = (log.info("Buy!") *> tradingStateRef.update(
-                                    _.toLongPosition.buyAt(bar.close)
-                                  )).when(shouldBuy(barsForBuy) & !tradingState.onLong)
-                                  val sellIf = {
-                                    for {
-                                      _       <- log.info("Sell!")
-                                      profit   = bar.close - tradingState.lastBuyRate
-                                      summary <- tradingStateRef.updateAndGet(
-                                                   _.toNeutralPosition.addSummary(profit)
-                                                 )
-                                      _       <-
-                                        log.info(
-                                          s"Profit: ${profit.toString} Summary: ${summary.tradeSummary.toString}"
-                                        )
-                                    } yield ()
-                                  }.when(shouldSell(barsForSell) & tradingState.onLong)
-
-                                  buyIf *> sellIf
-                                }.when(
-                                  barsForBuy.size >= buyContinuous & barsForSell.size >= sellContinuous
-                                )
-                              } yield ()
-                            }
+    signalStream       <- DowMethod(aggCount, buyContinuous, sellContinuous)
+                            .signal(transactionsStream)
+    _                  <- signalStream.foreach { signal =>
+                            for {
+                              tradingState <- tradingStateRef.get
+                              _            <- (log.info("Buy!") *> tradingStateRef.update(
+                                                _.toLongPosition.buyAt(signal.at)
+                                              )).when(signal.shouldBuy & !tradingState.onLong)
+                              _            <- {
+                                for {
+                                  _            <- log.info("Sell!")
+                                  tradingState <- tradingStateRef.get
+                                  profit        = signal.at - tradingState.lastBuyRate
+                                  summary      <- tradingStateRef.updateAndGet(
+                                                    _.toNeutralPosition.addSummary(profit)
+                                                  )
+                                  _            <-
+                                    log.info(
+                                      s"Profit: ${profit.toString} Summary: ${summary.tradeSummary.toString}"
+                                    )
+                                } yield ()
+                              }.when(signal.shouldSell & tradingState.onLong)
+                            } yield ()
+                          }
   } yield ()
 }
