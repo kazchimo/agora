d97190e09e0f568619985d23eb43bdca57b13a76
kazuki <pharcydetip@gmail.com>
2021-02-20 01:40:47 +0900

    feat: add CoincheckExchange stubLayer

--- domain/src/main/scala/domain/exchange/coincheck/package.scala
+++ domain/src/main/scala/domain/exchange/coincheck/package.scala
@@ -1,6 +1,7 @@
 package domain.exchange
 
 import domain.exchange.coincheck.CCOrder.CCOrderId
+import lib.error.ClientDomainError
 import sttp.client3.asynchttpclient.zio.SttpClient
 import zio._
 import zio.logging.Logging
@@ -25,5 +26,43 @@ package object coincheck {
         Stream[Nothing, CCPublicTransaction]
       ]
     }
+
+    val notStubbed: ZIO[Any, Throwable, Nothing] = ZIO.fail(
+      ClientDomainError("Access Stub object before method is property stubbed")
+    )
+
+    def stubLayer(
+      transactionsRes: RIO[SttpClient, Seq[CCTransaction]] = notStubbed,
+      ordersRes: RIO[SttpClient with zio.ZEnv, CCOrder] = notStubbed,
+      openOrdersRes: RIO[SttpClient, Seq[CCOrder]] = notStubbed,
+      cancelOrderRes: RIO[SttpClient, CCOrderId] = notStubbed,
+      cancelStatusRes: RIO[SttpClient, Boolean] = notStubbed,
+      publicTransactionsRes: ZIO[
+        SttpClient with zio.ZEnv with Logging,
+        Throwable,
+        Stream[Nothing, CCPublicTransaction]
+      ] = notStubbed
+    ): ULayer[CoincheckExchange] = ZLayer.succeed(new Service {
+      override def transactions: RIO[SttpClient, Seq[CCTransaction]] =
+        transactionsRes
+
+      override def orders(
+        order: CCOrderRequest
+      ): RIO[SttpClient with zio.ZEnv, CCOrder] = ordersRes
+
+      override def openOrders: RIO[SttpClient, Seq[CCOrder]] = openOrdersRes
+
+      override def cancelOrder(id: CCOrderId): RIO[SttpClient, CCOrderId] =
+        cancelOrderRes
+
+      override def cancelStatus(id: CCOrderId): RIO[SttpClient, Boolean] =
+        cancelStatusRes
+
+      override def publicTransactions: ZIO[
+        SttpClient with zio.ZEnv with Logging,
+        Throwable,
+        Stream[Nothing, CCPublicTransaction]
+      ] = publicTransactionsRes
+    })
   }
 }
