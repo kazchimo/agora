5bb11a3d2acbbed72c1a78dd49577b7155b54a7d
kazuki <pharcydetip@gmail.com>
2021-02-20 21:05:45 +0900

    feat: add dry wrtie layer

--- infra/src/main/scala/infra/exchange/ExchangeImpl.scala
+++ infra/src/main/scala/infra/exchange/ExchangeImpl.scala
@@ -4,14 +4,20 @@ import domain.conf.Conf
 import domain.exchange.bitflyer.BitflyerExchange
 import domain.exchange.coincheck.CoincheckExchange
 import infra.exchange.bitflyer.impl.BitflyerExchangeImpl
-import infra.exchange.coincheck.impl.CoinCheckExchangeImpl
-import zio.ZLayer
+import infra.exchange.coincheck.impl.{
+  CoinCheckExchangeImpl,
+  DryWriteCoincheckExchangeImpl
+}
+import zio.{ULayer, ZLayer}
 
 object ExchangeImpl {
   val coinCheckExchange: ZLayer[Conf, Throwable, CoincheckExchange] =
     ZLayer.fromFunctionM(conf =>
       conf.get.ccAccessKey.zipWith(conf.get.ccSecretKey)(CoinCheckExchangeImpl)
     )
+
+  val dryWriteCoinCheckExchange: ULayer[CoincheckExchange] =
+    ZLayer.succeed(DryWriteCoincheckExchangeImpl)
 
   val bitflyerExchange: ZLayer[Conf, Throwable, BitflyerExchange] =
     ZLayer.fromFunctionM(conf =>
