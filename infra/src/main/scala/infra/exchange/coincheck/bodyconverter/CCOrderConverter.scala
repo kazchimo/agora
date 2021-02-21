89be6f4296448a01d8ec081fdeaa934dca6b6ba6
kazuki <pharcydetip@gmail.com>
2021-02-21 19:45:29 +0900

    refactor: remove unused statements

--- infra/src/main/scala/infra/exchange/coincheck/bodyconverter/CCOrderConverter.scala
+++ infra/src/main/scala/infra/exchange/coincheck/bodyconverter/CCOrderConverter.scala
@@ -1,16 +1,13 @@
 package infra.exchange.coincheck.bodyconverter
 
 import domain.exchange.coincheck.CCOrderRequest.{
-  CCOrderPair,
   CCOrderRequestAmount,
-  CCOrderRequestRate,
-  CCOrderType
+  CCOrderRequestRate
 }
 import domain.exchange.coincheck._
 import io.circe.Encoder
 import io.circe.generic.extras.Configuration
 import io.circe.syntax._
-import enumeratum._
 
 object CCOrderConverter {
   implicit val codecConfig: Configuration =
@@ -18,16 +15,6 @@ object CCOrderConverter {
 
   implicit def encodeOrderRequest[T]: Encoder[CCOrderRequest[T]] =
     Encoder.instance(_.asJson)
-
-//  implicitly[Encoder[CCOrderPair]]
-
-  private val orderType       = "order_type"
-  private val rate            = "rate"
-  private val amount          = "amount"
-  private val marketBuyAmount = "market_buy_amount"
-  private val stopLossRate    = "stop_loss_rate"
-  private val pair            = "pair"
-  private val btcJpy          = "btc_jpy"
 
   implicit val ccOrderRequestRateEncoder: Encoder[CCOrderRequestRate] =
     Encoder.instance(_.value.value.asJson)
