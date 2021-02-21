3ff7e58152e17e9ffd4facb67ded06796fd6bf69
kazuki <pharcydetip@gmail.com>
2021-02-21 14:47:23 +0900

    feat: add CCBalance.fromRaw

--- domain/src/main/scala/domain/exchange/coincheck/CCBalance.scala
+++ domain/src/main/scala/domain/exchange/coincheck/CCBalance.scala
@@ -4,11 +4,19 @@ import domain.exchange.coincheck.CCBalance.{BtcAmount, JpyAmount}
 import domain.lib.VOFactory
 import eu.timepit.refined.numeric.NonNegative
 import io.estatico.newtype.macros.newtype
+import lib.error.ClientDomainError
 import lib.refined.NonNegativeDouble
+import zio.IO
 
 final case class CCBalance(jpy: JpyAmount, btc: BtcAmount)
 
 object CCBalance {
+  def fromRaw(jpy: Double, btc: Double): IO[ClientDomainError, CCBalance] =
+    for {
+      jpy <- JpyAmount(jpy)
+      btc <- BtcAmount(btc)
+    } yield CCBalance(jpy, btc)
+
   @newtype case class JpyAmount(value: NonNegativeDouble)
   object JpyAmount extends VOFactory[Double, NonNegative] {
     override type VO = JpyAmount
