88263c8aa8abca8c90017d59beee4d2cc9e72998
kazuki <pharcydetip@gmail.com>
2021-02-21 14:45:34 +0900

    feat: add factory to amounts

--- domain/src/main/scala/domain/exchange/coincheck/CCBalance.scala
+++ domain/src/main/scala/domain/exchange/coincheck/CCBalance.scala
@@ -1,6 +1,8 @@
 package domain.exchange.coincheck
 
 import domain.exchange.coincheck.CCBalance.{BtcAmount, JpyAmount}
+import domain.lib.VOFactory
+import eu.timepit.refined.numeric.NonNegative
 import io.estatico.newtype.macros.newtype
 import lib.refined.NonNegativeDouble
 
@@ -8,5 +10,12 @@ final case class CCBalance(jpy: JpyAmount, btc: BtcAmount)
 
 object CCBalance {
   @newtype case class JpyAmount(value: NonNegativeDouble)
+  object JpyAmount extends VOFactory[Double, NonNegative] {
+    override type VO = JpyAmount
+  }
+
   @newtype case class BtcAmount(value: NonNegativeDouble)
+  object BtcAmount extends VOFactory[Double, NonNegative] {
+    override type VO = BtcAmount
+  }
 }
