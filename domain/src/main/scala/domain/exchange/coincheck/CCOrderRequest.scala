e1152786ba9e7ed0050368d7f4728e5be22c28fa
kazuki <pharcydetip@gmail.com>
2021-02-21 18:33:01 +0900

    refactor: remodeling coincheck order request

--- domain/src/main/scala/domain/exchange/coincheck/CCOrderRequest.scala
+++ domain/src/main/scala/domain/exchange/coincheck/CCOrderRequest.scala
@@ -1,130 +1,58 @@
 package domain.exchange.coincheck
 
-import domain.exchange.coincheck.CCLimitOrderRequest.{
+import domain.exchange.coincheck.CCOrderRequest.{
+  CCOrderPair,
   CCOrderRequestAmount,
-  CCOrderRequestRate
+  CCOrderRequestRate,
+  CCOrderType
 }
-import domain.exchange.coincheck.CCMarketBuyRequest.CCMarketBuyRequestAmount
 import domain.lib.VOFactory
-import eu.timepit.refined.api.Refined
+import enumeratum.EnumEntry.Snakecase
+import enumeratum._
 import eu.timepit.refined.numeric.Positive
 import io.estatico.newtype.macros.newtype
-import lib.error.ClientDomainError
-import zio.IO
+import lib.refined.PositiveDouble
 
 // about order -> https://coincheck.com/ja/documents/exchange/api#order-new
 // about stop order -> https://faq.coincheck.com/s/article/40203?language=ja
 
-sealed trait CCOrderRequest
+final case class CCOrderRequest private (
+  pair: CCOrderPair,
+  orderType: CCOrderType,
+  rate: Option[CCOrderRequestRate],
+  amount: Option[CCOrderRequestAmount],
+  marketBuyAmount: Option[CCOrderRequestAmount],
+  stopLossRate: Option[CCOrderRequestRate]
+)
 
-sealed abstract class CCLimitOrderRequest(
-  val rate: CCOrderRequestRate,
-  val amount: CCOrderRequestAmount
-) extends CCOrderRequest {
-  def changeAmount(amount: CCOrderRequestAmount): CCLimitOrderRequest
+object CCOrderRequest {
+  sealed trait CCOrderPair extends Snakecase
+  object CCOrderPair       extends Enum[CCOrderPair] {
+    val values: IndexedSeq[CCOrderPair] = findValues
 
-  def changeRate(rate: CCOrderRequestRate): CCLimitOrderRequest
-}
+    case object BtcJpy  extends CCOrderPair
+    case object EtcJpy  extends CCOrderPair
+    case object FctJpy  extends CCOrderPair
+    case object MonaJpy extends CCOrderPair
+  }
 
-object CCLimitOrderRequest {
-  @newtype case class CCOrderRequestRate(value: Double Refined Positive)
+  sealed trait CCOrderType extends Snakecase
+  object CCOrderType       extends Enum[CCOrderType] {
+    val values: IndexedSeq[CCOrderType] = findValues
+
+    case object Buy        extends CCOrderType
+    case object Sell       extends CCOrderType
+    case object MarketBuy  extends CCOrderType
+    case object MarketSell extends CCOrderType
+  }
+
+  @newtype case class CCOrderRequestRate(value: PositiveDouble)
   object CCOrderRequestRate extends VOFactory[Double, Positive] {
     override type VO = CCOrderRequestRate
   }
 
-  @newtype case class CCOrderRequestAmount(value: Double Refined Positive)
+  @newtype case class CCOrderRequestAmount(value: PositiveDouble)
   object CCOrderRequestAmount extends VOFactory[Double, Positive] {
     override type VO = CCOrderRequestAmount
   }
 }
-
-final case class CCLimitBuyRequest(
-  override val rate: CCOrderRequestRate,
-  override val amount: CCOrderRequestAmount
-) extends CCLimitOrderRequest(rate, amount) {
-  override def changeAmount(amount: CCOrderRequestAmount): CCLimitOrderRequest =
-    this.copy(amount = amount)
-
-  override def changeRate(rate: CCOrderRequestRate): CCLimitOrderRequest =
-    this.copy(rate = rate)
-}
-
-object CCLimitBuyRequest {
-  def fromRaw(
-    rate: Double,
-    amount: Double
-  ): IO[ClientDomainError, CCLimitBuyRequest] = for {
-    r <- CCOrderRequestRate(rate)
-    a <- CCOrderRequestAmount(amount)
-  } yield CCLimitBuyRequest(r, a)
-}
-
-final case class CCLimitStopBuyRequest(
-  override val rate: CCOrderRequestRate,
-  stopLossRate: CCOrderRequestRate,
-  override val amount: CCOrderRequestAmount
-) extends CCLimitOrderRequest(rate, amount) {
-  override def changeAmount(amount: CCOrderRequestAmount): CCLimitOrderRequest =
-    this.copy(amount = amount)
-
-  override def changeRate(rate: CCOrderRequestRate): CCLimitOrderRequest =
-    this.copy(rate = rate)
-}
-
-final case class CCLimitSellRequest(
-  override val rate: CCOrderRequestRate,
-  override val amount: CCOrderRequestAmount
-) extends CCLimitOrderRequest(rate, amount) {
-  override def changeAmount(amount: CCOrderRequestAmount): CCLimitOrderRequest =
-    this.copy(amount = amount)
-
-  override def changeRate(rate: CCOrderRequestRate): CCLimitOrderRequest =
-    this.copy(rate = rate)
-}
-
-object CCLimitSellRequest {
-  def fromRaw(
-    rate: Double,
-    amount: Double
-  ): IO[ClientDomainError, CCLimitSellRequest] = for {
-    r <- CCOrderRequestRate(rate)
-    a <- CCOrderRequestAmount(amount)
-  } yield CCLimitSellRequest(r, a)
-}
-
-final case class CCLimitStopSellRequest(
-  override val rate: CCOrderRequestRate,
-  stopLossRate: CCOrderRequestRate,
-  override val amount: CCOrderRequestAmount
-) extends CCLimitOrderRequest(rate, amount) {
-  override def changeAmount(amount: CCOrderRequestAmount): CCLimitOrderRequest =
-    this.copy(amount = amount)
-
-  override def changeRate(rate: CCOrderRequestRate): CCLimitOrderRequest =
-    this.copy(rate = rate)
-}
-
-sealed trait CCMarketOrderRequest extends CCOrderRequest
-
-final case class CCMarketBuyRequest(marketBuyAmount: CCMarketBuyRequestAmount)
-    extends CCMarketOrderRequest
-
-final case class CCMarketStopBuyRequest(
-  stopLossRate: CCOrderRequestRate,
-  marketBuyAmount: CCMarketBuyRequestAmount
-) extends CCMarketOrderRequest
-
-object CCMarketBuyRequest {
-  @newtype case class CCMarketBuyRequestAmount(value: Double Refined Positive)
-  object CCMarketBuyRequestAmount extends VOFactory[Double, Positive] {
-    override type VO = CCMarketBuyRequestAmount
-  }
-}
-
-final case class CCMarketSellRequest(amount: CCOrderRequestAmount)
-    extends CCMarketOrderRequest
-
-final case class CCMarketStopSellRequest(
-  stopLossRate: CCOrderRequestRate,
-  amount: CCOrderRequestAmount
-) extends CCMarketOrderRequest
