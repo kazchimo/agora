9845969a53fff44dc9dda296a06a8fec4c834eac
kazuki <pharcydetip@gmail.com>
2021-02-22 00:06:49 +0900

    feat: impl DryBalance

--- infra/src/main/scala/infra/exchange/coincheck/impl/DryBalance.scala
+++ infra/src/main/scala/infra/exchange/coincheck/impl/DryBalance.scala
@@ -1,13 +1,15 @@
 package infra.exchange.coincheck.impl
 
 import domain.conf.Conf
-import domain.exchange.coincheck.{CCBalance, CoincheckExchange}
+import domain.exchange.coincheck.CCBalance
 import sttp.client3.asynchttpclient.zio.SttpClient
 import zio.RIO
 import zio.logging.Logging
 
 private[coincheck] trait DryBalance extends AuthStrategy {
-  self: CoincheckExchange.Service =>
+  self: DryCoincheckExchangeImpl =>
+
   final override def balance
-    : RIO[SttpClient with Conf with Logging, CCBalance] = ???
+    : RIO[SttpClient with Conf with Logging, CCBalance] =
+    CCBalance.fromRaw(fakeExchange.jpy, fakeExchange.btc)
 }
