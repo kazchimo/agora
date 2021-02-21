71f3b5885f90f3294ec4b65a4aa1cd0f80c5804f
kazuki <pharcydetip@gmail.com>
2021-02-21 21:18:44 +0900

    fix: add correct encoder

--- infra/src/main/scala/infra/exchange/coincheck/bodyconverter/CCOrderConverter.scala
+++ infra/src/main/scala/infra/exchange/coincheck/bodyconverter/CCOrderConverter.scala
@@ -1,20 +1,30 @@
 package infra.exchange.coincheck.bodyconverter
 
-import domain.exchange.coincheck.CCOrderRequest.{
-  CCOrderRequestAmount,
-  CCOrderRequestRate
-}
+import domain.exchange.coincheck.CCOrderRequest._
 import domain.exchange.coincheck._
-import io.circe.Encoder
 import io.circe.generic.extras.Configuration
 import io.circe.syntax._
+import io.circe.{Encoder, Json}
 
 object CCOrderConverter {
   implicit val codecConfig: Configuration =
     Configuration.default.withSnakeCaseMemberNames
 
-  implicit def encodeOrderRequest[T]: Encoder[CCOrderRequest[T]] =
-    Encoder.instance(_.asJson)
+  implicit def encodeOrderRequest[T <: CCOrderType]
+    : Encoder[CCOrderRequest[T]] = Encoder.instance { req =>
+    Json
+      .obj(
+        "pair"              -> req.pair.asJson,
+        "order_type"        -> req.orderType.asJson,
+        "rate"              -> req.rate.asJson,
+        "amount"            -> req.amount.asJson,
+        "market_buy_amount" -> req.marketBuyAmount.asJson,
+        "stop_loss_rate"    -> req.stopLossRate.asJson
+      ).dropNullValues
+  }
+
+  implicit def encodeOrderType[T <: CCOrderType]: Encoder[T] =
+    Encoder.instance(_.entryName.asJson)
 
   implicit val ccOrderRequestRateEncoder: Encoder[CCOrderRequestRate] =
     Encoder.instance(_.value.value.asJson)
