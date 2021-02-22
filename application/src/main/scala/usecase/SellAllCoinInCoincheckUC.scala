9646f42b44c2a9d8e67026371017c53f8b3fa971
kazuki <pharcydetip@gmail.com>
2021-02-23 03:16:07 +0900

    refactor: add type

--- application/src/main/scala/usecase/SellAllCoinInCoincheckUC.scala
+++ application/src/main/scala/usecase/SellAllCoinInCoincheckUC.scala
@@ -1,14 +1,15 @@
 package usecase
 
+import domain.AllEnv
 import domain.broker.coincheck.CoincheckBroker
 import domain.exchange.coincheck.{CCOrderRequest, CoincheckExchange}
 import lib.error.AdaptorInternalError
 import lib.syntax.all._
-import zio.ZIO
+import zio.{RIO, ZIO}
 import zio.logging.log
 
 object SellAllCoinInCoincheckUC {
-  def sell(updatePriceIntervalSec: Int) = for {
+  def sell(updatePriceIntervalSec: Int): RIO[AllEnv, Unit] = for {
     (balance, transactions) <-
       CoincheckExchange.balance.zipPar(CoincheckExchange.transactions)
     transaction             <- ZIO
