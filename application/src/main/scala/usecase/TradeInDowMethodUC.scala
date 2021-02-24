f73bf317a10c23c382c3981b15fc60ddf022191d
kazuki <pharcydetip@gmail.com>
2021-02-24 16:46:35 +0900

    refactor: define LimitOrder show

--- application/src/main/scala/usecase/TradeInDowMethodUC.scala
+++ application/src/main/scala/usecase/TradeInDowMethodUC.scala
@@ -1,11 +1,14 @@
 package usecase
 
+import cats.Show
 import domain.broker.coincheck.CoincheckBroker
+import domain.exchange.coincheck.CCOrder.LimitOrder
 import domain.exchange.coincheck.{CCOrderRequest, CoincheckExchange}
 import domain.strategy.{DowMethod, Signal}
 import lib.syntax.all._
 import zio.Ref
 import zio.logging.log
+import cats.syntax.show._
 
 final case class TradingState(
   onLong: Boolean,
@@ -26,24 +29,23 @@ object TradeInDowMethodUC {
   val jpy      = 50000
   val interval = 10
 
+  implicit def orderRequestShow[T <: LimitOrder]
+    : Show[CCOrderRequest[T]] = Show.show(o =>
+    s"OrderRequest: rate=${o.limitRate.toString} amount=${o.limitAmount.toString}"
+  )
+
   private def buy(signal: Signal, tradingStateRef: Ref[TradingState]) = for {
-    _       <-
-      log.info(
-        s"Buy! rate=${signal.at.toString} amount=${(jpy / signal.at).toString}"
-      )
     request <- CCOrderRequest.limitBuy(signal.at, jpy / signal.at)
+    _       <- log.info(s"Buy! ${request.show}")
     _       <- CoincheckBroker().priceAdjustingOrder(request, interval)
     _       <- tradingStateRef.update(_.toLongPosition.buyAt(signal.at))
   } yield ()
 
   private def sell(signal: Signal, tradingStateRef: Ref[TradingState]) = for {
     tradingState <- tradingStateRef.get
-    _            <-
-      log.info(
-        s"Sell! rate=${signal.at.toString} amount=${(jpy / tradingState.lastBuyRate).toString}"
-      )
     request      <-
       CCOrderRequest.limitSell(signal.at, jpy / tradingState.lastBuyRate)
+    _            <- log.info(s"Sell! ${request.show}")
     _            <- CoincheckBroker().priceAdjustingOrder(request, interval)
     tradingState <- tradingStateRef.get
     profit        =
