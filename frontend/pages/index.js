4b15f8599771f12cc307586735c1ebdff2fda18c
kazuki <pharcydetip@gmail.com>
2021-03-09 12:31:25 +0900

    feat: tutorial

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from "next/link"
 
 export default function Home() {
   return (
@@ -10,9 +11,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Read <Link href={"/posts/first-post"}>this page</Link></h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
---
+++ frontend/pages/posts/first-post.tsx
@@ -0,0 +1,5 @@
+const FirstPost = () => {
+    return <h1>unchi</h1>
+}
+
+export default FirstPost
