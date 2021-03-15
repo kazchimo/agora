bd271e3d2d81048ea0fab8a87094f2a3c7f911b8
kazuki <pharcydetip@gmail.com>
2021-03-16 02:34:43 +0900

    feat: add liquic exchange layer

--- backend/infra/src/main/scala/infra/exchange/ExchangeImpl.scala
+++ backend/infra/src/main/scala/infra/exchange/ExchangeImpl.scala
@@ -4,11 +4,13 @@ import domain.conf.Conf
 import domain.exchange.bitflyer.BitflyerExchange
 import domain.exchange.coincheck.CCOrder.CCOrderRate
 import domain.exchange.coincheck.CoincheckExchange
+import domain.exchange.liquid.LiquidExchange
 import infra.exchange.bitflyer.impl.BitflyerExchangeImpl
 import infra.exchange.coincheck.impl.{
   CoinCheckExchangeImpl,
   DryWriteCoincheckExchangeImpl
 }
+import infra.exchange.liquid.impl.LiquidExchangeImpl
 import zio.{ULayer, ZLayer}
 
 object ExchangeImpl {
@@ -26,4 +28,7 @@ object ExchangeImpl {
     ZLayer.fromFunctionM(conf =>
       conf.get.bfAccessKey.zipWith(conf.get.bfSecretKey)(BitflyerExchangeImpl)
     )
+
+  val liquidExchange: ULayer[LiquidExchange] =
+    ZLayer.succeed(LiquidExchangeImpl())
 }
