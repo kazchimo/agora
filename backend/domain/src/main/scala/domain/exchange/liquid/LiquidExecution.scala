53371e45f8dd9fc74b40a07d65b8f92e235a36ea
kazuki <pharcydetip@gmail.com>
2021-03-17 16:23:23 +0900

    feat: add LiquidExecution

---
+++ backend/domain/src/main/scala/domain/exchange/liquid/LiquidExecution.scala
@@ -0,0 +1,38 @@
+package domain.exchange.liquid
+
+import domain.exchange.liquid.LiquidExecution._
+import domain.lib.{EnumZio, VOFactory}
+import enumeratum._
+import io.estatico.newtype.macros.newtype
+import lib.refined.{NonNegativeDouble, NonNegativeLong}
+
+final case class LiquidExecution(
+  id: Id,
+  quantity: Quantity,
+  price: Price,
+  takerSide: TakerSide
+)
+
+object LiquidExecution {
+  @newtype case class Id(value: NonNegativeLong)
+  object Id extends VOFactory
+
+  @newtype case class Quantity(value: NonNegativeDouble)
+  object Quantity extends VOFactory
+
+  @newtype case class Price(value: NonNegativeDouble)
+  object Price extends VOFactory
+
+  sealed abstract class TakerSide(override val entryName: String)
+      extends EnumEntry
+
+  object TakerSide extends Enum[TakerSide] with EnumZio[TakerSide] {
+    val values: IndexedSeq[TakerSide] = findValues
+
+    case object Sell extends TakerSide("sell")
+    case object Buy  extends TakerSide("buy")
+  }
+
+  @newtype case class CreatedAt(value: NonNegativeLong)
+  object CreatedAt extends VOFactory
+}
