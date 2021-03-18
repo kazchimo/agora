a1e98307bbdc9bb8f026b517e823402f5f3a1b95
kazuki <pharcydetip@gmail.com>
2021-03-18 23:53:08 +0900

    fix: type

--- backend/application/src/main/scala/usecase/coincheck/SellAllCoinInCoincheckUC.scala
+++ backend/application/src/main/scala/usecase/coincheck/SellAllCoinInCoincheckUC.scala
@@ -2,6 +2,7 @@ package usecase.coincheck
 
 import domain.AllEnv
 import domain.broker.coincheck.CoincheckBroker
+import domain.exchange.coincheck.CCOrder.{CCOrderAmount, CCOrderRate}
 import domain.exchange.coincheck.{CCOrderRequest, CoincheckExchange}
 import lib.error.AdaptorInternalError
 import lib.syntax.all._
@@ -16,8 +17,8 @@ object SellAllCoinInCoincheckUC {
                                  .fromOption(transactions.headOption).orElseFail(
                                    AdaptorInternalError("Could not find latest price!")
                                  )
-    req                     <- CCOrderRequest
-                                 .limitSell(transaction.rate.deepInnerV, balance.btc.deepInnerV)
+    amount                  <- CCOrderAmount(balance.btc.deepInnerV)
+    req                      = CCOrderRequest.limitSell(CCOrderRate(transaction.rate.value), amount)
     _                       <- log.info(s"Sell All btc at ${req.toString}!")
     _                       <- CoincheckBroker()
                                  .priceAdjustingOrder(req, updatePriceIntervalSec).when(
