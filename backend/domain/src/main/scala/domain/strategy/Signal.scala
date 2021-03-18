6a1efaf625dd89fb78c5d3b756b3f3ef682faef6
kazuki <pharcydetip@gmail.com>
2021-03-18 23:07:32 +0900

    feat: use Refined in Signal

--- backend/domain/src/main/scala/domain/strategy/Signal.scala
+++ backend/domain/src/main/scala/domain/strategy/Signal.scala
@@ -1,15 +1,17 @@
 package domain.strategy
 
-sealed abstract class Signal(val at: Double) {
+import lib.refined.PositiveDouble
+
+sealed abstract class Signal(val at: PositiveDouble) {
   def shouldBuy: Boolean
 
   final def shouldSell: Boolean = !shouldBuy
 }
 
-final case class Buy(override val at: Double) extends Signal(at) {
+final case class Buy(override val at: PositiveDouble) extends Signal(at) {
   override def shouldBuy: Boolean = true
 }
 
-final case class Sell(override val at: Double) extends Signal(at) {
+final case class Sell(override val at: PositiveDouble) extends Signal(at) {
   override def shouldBuy: Boolean = false
 }
