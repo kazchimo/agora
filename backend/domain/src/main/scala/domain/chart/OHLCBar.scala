4fcd842a5feaf68425ee0a3e81bc1e28ec95f20c
kazuki <pharcydetip@gmail.com>
2021-03-18 23:06:44 +0900

    feat: use Refined in OHLCBar

--- backend/domain/src/main/scala/domain/chart/OHLCBar.scala
+++ backend/domain/src/main/scala/domain/chart/OHLCBar.scala
@@ -1,23 +1,38 @@
 package domain.chart
 
+import domain.chart.OHLCBar._
 import domain.exchange.coincheck.CCPublicTransaction
+import domain.lib.VOFactory
+import io.estatico.newtype.macros.newtype
+import lib.instance.all._
+import lib.refined.PositiveDouble
 import lib.syntax.all._
 import zio.Chunk
 
-final case class OHLCBar(
-  open: Double,
-  high: Double,
-  low: Double,
-  close: Double
-) {
-  def range: Double = high - low
+final case class OHLCBar(open: Open, high: High, low: Low, close: Close) {
+  def range: Double = high.deepInnerV - low.deepInnerV
 }
 
 object OHLCBar {
+  @newtype case class Open(value: PositiveDouble)
+  object Open extends VOFactory
+
+  @newtype case class High(value: PositiveDouble)
+  object High extends VOFactory
+
+  @newtype case class Low(value: PositiveDouble)
+  object Low extends VOFactory
+
+  @newtype case class Close(value: PositiveDouble)
+  object Close extends VOFactory
+
   def fromTransactions(ts: Chunk[CCPublicTransaction]): OHLCBar = {
-    val rates = ts.map(_.rate.deepInnerV)
-    val max   = rates.max
-    val min   = rates.min
-    OHLCBar(ts.head.rate.deepInnerV, max, min, ts.last.rate.deepInnerV)
+    val rates = ts.map(_.rate.value)
+    OHLCBar(
+      Open(ts.head.rate.value),
+      High(rates.max),
+      Low(rates.min),
+      Close(ts.last.rate.value)
+    )
   }
 }
