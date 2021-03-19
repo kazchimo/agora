456c24970e57a3193c8c5039891aacb599f7d4c6
kazuki <pharcydetip@gmail.com>
2021-03-19 17:01:08 +0900

    feat: add order type

--- backend/domain/src/main/scala/domain/exchange/liquid/LiquidOrder.scala
+++ backend/domain/src/main/scala/domain/exchange/liquid/LiquidOrder.scala
@@ -1,7 +1,8 @@
 package domain.exchange.liquid
 
 import domain.exchange.liquid.LiquidOrder._
-import domain.lib.VOFactory
+import domain.lib.{VOFactory, ZEnum}
+import enumeratum.EnumEntry.Snakecase
 import io.estatico.newtype.macros.newtype
 import lib.refined.PositiveDouble
 
@@ -13,4 +14,16 @@ object LiquidOrder {
 
   @newtype case class Quantity(value: PositiveDouble)
   object Quantity extends VOFactory
+
+  sealed trait OrderType extends Snakecase
+  object OrderType       extends ZEnum[OrderType] {
+    override def values: IndexedSeq[OrderType] = findValues
+
+    case object Limit           extends OrderType
+    case object Market          extends OrderType
+    case object MarketWithRange extends OrderType
+    case object TrailingStop    extends OrderType
+    case object LimitPostOnly   extends OrderType
+    case object Stop            extends OrderType
+  }
 }
