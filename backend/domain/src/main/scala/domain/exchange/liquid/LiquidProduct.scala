1e297d1171584b28416bd64b7cb3e6f4c56fb398
kazuki <pharcydetip@gmail.com>
2021-03-19 17:23:57 +0900

    feat: add id to LiquidProduct and define as abstract class

--- backend/domain/src/main/scala/domain/exchange/liquid/LiquidProduct.scala
+++ backend/domain/src/main/scala/domain/exchange/liquid/LiquidProduct.scala
@@ -1,22 +1,33 @@
 package domain.exchange.liquid
 
 import domain.exchange.liquid.LiquidProduct.{
+  Id,
   LastTradedPrice,
   LastTradedQuantity
 }
 import domain.lib.VOFactory
 import io.estatico.newtype.macros.newtype
-import lib.refined.NonNegativeDouble
+import lib.refined.{NonNegativeDouble, PositiveLong}
 
-final case class LiquidProduct(
-  lastTradedPrice: LastTradedPrice,
-  lastTradedQuantity: LastTradedQuantity
+sealed abstract class LiquidProduct(
+  val id: Id,
+  val lastTradedPrice: LastTradedPrice,
+  val lastTradedQuantity: LastTradedQuantity
 )
 
 object LiquidProduct {
+  @newtype case class Id(value: PositiveLong)
+  object Id extends VOFactory
+
   @newtype case class LastTradedPrice(value: NonNegativeDouble)
   object LastTradedPrice extends VOFactory
 
   @newtype case class LastTradedQuantity(value: NonNegativeDouble)
   object LastTradedQuantity extends VOFactory
+
 }
+
+final case class BtcJpy(
+  override val lastTradedPrice: LastTradedPrice,
+  override val lastTradedQuantity: LastTradedQuantity
+) extends LiquidProduct(Id.unsafeFrom(5L), lastTradedPrice, lastTradedQuantity)
