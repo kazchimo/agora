03980e0866247778a52df40638b894e0d3e476a7
kazuki <pharcydetip@gmail.com>
2021-03-20 19:47:19 +0900

    refactor: add type alias

--- backend/domain/src/main/scala/domain/broker/coincheck/liquid/LiquidBroker.scala
+++ backend/domain/src/main/scala/domain/broker/coincheck/liquid/LiquidBroker.scala
@@ -3,6 +3,7 @@ package domain.broker.coincheck.liquid
 import domain.AllEnv
 import domain.exchange.liquid.LiquidOrder.{Id, Price, Side}
 import domain.exchange.liquid.{LiquidExchange, LiquidOrder, OrderOnBook}
+import lib.zio.EStream
 import zio.duration._
 import zio.stream.Stream
 import zio.{RIO, Ref, ZIO}
@@ -13,24 +14,23 @@ object LiquidBroker {
     waitFilledUntil(id, Duration.Infinity)
 
   def waitFilledUntil(id: Id, d: Duration): ZIO[AllEnv, Throwable, Unit] = for {
-    stream: Stream[Throwable, LiquidOrder] <- LiquidExchange.ordersStream
-    _                                      <- stream
-                                                .interruptAfter(d).foreachWhile(o =>
-                                                  ZIO.succeed(
-                                                    o.notFilled
-                                                      || o.id != id
-                                                  )
-                                                )
+    stream: EStream[LiquidOrder] <- LiquidExchange.ordersStream
+    _                            <- stream
+                                      .interruptAfter(d).foreachWhile(o =>
+                                        ZIO.succeed(
+                                          o.notFilled
+                                            || o.id != id
+                                        )
+                                      )
   } yield ()
 
   def latestHeadPriceRef(side: Side): RIO[AllEnv, Ref[Option[Price]]] = for {
-    stream: Stream[Throwable, Seq[OrderOnBook]] <-
-      LiquidExchange.orderBookStream(side)
-    ref                                         <- Ref.make[Option[Price]](None)
-    _                                           <- stream.foreach { os =>
-                                                     ZIO
-                                                       .getOrFail(os.headOption)
-                                                       .flatMap(order => ref.set(Some(order.price)))
-                                                   }.fork
+    stream: EStream[Seq[OrderOnBook]] <- LiquidExchange.orderBookStream(side)
+    ref                               <- Ref.make[Option[Price]](None)
+    _                                 <- stream.foreach { os =>
+                                           ZIO
+                                             .getOrFail(os.headOption)
+                                             .flatMap(order => ref.set(Some(order.price)))
+                                         }.fork
   } yield ref
 }
--- backend/lib/src/main/scala/lib/zio/package.scala
+++ backend/lib/src/main/scala/lib/zio/package.scala
@@ -1,8 +1,11 @@
 package lib
 
 import _root_.zio.ZRef
+import _root_.zio.stream._
 
 package object zio {
   type UReadOnlyRef[+A]  = ZRef[Nothing, Nothing, Nothing, A]
   type UWriteOnlyRef[-A] = ZRef[Nothing, Unit, A, Nothing]
+
+  type EStream[+A] = Stream[Throwable, A]
 }
