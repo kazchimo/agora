f86b34e843844236e82da79d491e5edaff2b4356
kazuki <pharcydetip@gmail.com>
2021-03-20 18:21:13 +0900

    feat: impl orders stream

--- backend/infra/src/main/scala/infra/exchange/liquid/impl/OrdersStream.scala
+++ backend/infra/src/main/scala/infra/exchange/liquid/impl/OrdersStream.scala
@@ -2,10 +2,28 @@ package infra.exchange.liquid.impl
 
 import domain.AllEnv
 import domain.exchange.liquid.{LiquidExchange, LiquidOrder}
-import zio.{RIO, stream}
+import infra.exchange.liquid.response.OrderResponse
+import sttp.ws.WebSocket
+import zio.{Queue, RIO, ZIO, stream}
+import zio.stream.Stream
+import io.circe.parser.decode
+import io.circe.refined._
+import io.circe.generic.auto._
 
 private[liquid] trait OrdersStream extends WebSocketHandler {
   self: LiquidExchange.Service =>
+  private def useWS(queue: Queue[LiquidOrder])(ws: WebSocket[RIO[AllEnv, *]]) =
+    handleAuthMessage(ws, "user_account_jpy_orders", "updated") { d =>
+      for {
+        res   <- ZIO.fromEither(decode[OrderResponse](d.data))
+        order <- res.toOrder
+        _     <- queue.offer(order)
+      } yield ()
+    }
+
   override def ordersStream
-    : RIO[AllEnv, stream.Stream[Throwable, LiquidOrder]] = ???
+    : RIO[AllEnv, stream.Stream[Throwable, LiquidOrder]] = for {
+    queue <- Queue.unbounded[LiquidOrder]
+    fiber <- sendWS(useWS(queue)).fork
+  } yield Stream.fromQueueWithShutdown(queue).interruptWhen(fiber.join)
 }
