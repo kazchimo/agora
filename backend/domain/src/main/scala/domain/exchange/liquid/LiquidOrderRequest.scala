b3c5705dfd0fb71b40d7212b4e71caab25e38856
kazuki <pharcydetip@gmail.com>
2021-03-20 18:54:53 +0900

    refactor: import

--- backend/domain/src/main/scala/domain/exchange/liquid/LiquidOrderRequest.scala
+++ backend/domain/src/main/scala/domain/exchange/liquid/LiquidOrderRequest.scala
@@ -2,25 +2,22 @@ package domain.exchange.liquid
 
 import domain.exchange.liquid.LiquidOrder.OrderType.Limit
 import domain.exchange.liquid.LiquidOrder.Side.{Buy, Sell}
-import domain.exchange.liquid.LiquidOrder.{OrderType, Quantity}
+import domain.exchange.liquid.LiquidOrder._
 
 // https://developers.liquid.com/#create-an-order
-final case class LiquidOrderRequest[
-  O <: OrderType,
-  S <: LiquidOrder.Side
-] private (
+final case class LiquidOrderRequest[O <: OrderType, S <: Side] private (
   orderType: O,
   productId: LiquidProduct.Id,
   side: S,
-  quantity: LiquidOrder.Quantity,
-  price: Option[LiquidOrder.Price] = None
+  quantity: Quantity,
+  price: Option[Price] = None
 )
 
 object LiquidOrderRequest {
   def limitBuy(
     productId: LiquidProduct.Id,
     quantity: Quantity,
-    price: LiquidOrder.Price
+    price: Price
   ): LiquidOrderRequest[OrderType.Limit, Buy] =
     LiquidOrderRequest(Limit, productId, Buy, quantity, Some(price))
 
