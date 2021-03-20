e10882d58c3c97304ab6a3d3732769a3c6182270
kazuki <pharcydetip@gmail.com>
2021-03-20 17:19:52 +0900

    feat: impl auth websocket

--- backend/infra/src/main/scala/infra/exchange/liquid/impl/WebSocketHandler.scala
+++ backend/infra/src/main/scala/infra/exchange/liquid/impl/WebSocketHandler.scala
@@ -1,6 +1,8 @@
 package infra.exchange.liquid.impl
 
 import domain.AllEnv
+import domain.conf.Conf
+import domain.exchange.Nonce
 import domain.exchange.liquid.LiquidExchange
 import infra.exchange.liquid.Endpoints
 import io.circe.Json
@@ -12,11 +14,15 @@ import sttp.client3.{Response, UriContext, asWebSocketAlways, basicRequest}
 import sttp.ws.{WebSocket, WebSocketClosed, WebSocketFrame}
 import zio.logging.log
 import zio.{RIO, ZIO}
+import lib.instance.all._
+import pdi.jwt.Jwt
+import lib.syntax.all._
 
 private[liquid] case class WSMessage(event: String)
 private[liquid] case class DataMessage(event: String, data: String)
 
-private[liquid] trait WebSocketHandler { self: LiquidExchange.Service =>
+private[liquid] trait WebSocketHandler extends AuthRequest {
+  self: LiquidExchange.Service =>
   private def subscribeText(channel: String) = WebSocketFrame.text(
     Json
       .obj(
@@ -24,6 +30,28 @@ private[liquid] trait WebSocketHandler { self: LiquidExchange.Service =>
         "data"  -> Json.obj("channel" -> channel.asJson)
       ).asJson.noSpaces
   )
+
+  private def authText = for {
+    tokenId <- Conf.liquidTokenId
+    sec     <- Conf.liquidSecret
+    nonce   <- Nonce.getNonce
+    payload  = Json
+                 .obj(
+                   "token_id" -> tokenId.asJson,
+                   "path"     -> "/realtime".asJson,
+                   "nonce"    -> nonce.asJson
+                 ).noSpaces
+    text     = Json.obj(
+                 "event" -> "quoine:auth_request".asJson,
+                 "data"  -> Json.obj(
+                   "headers" -> Json.obj(
+                     "X-Quoine-Auth"
+                       -> Jwt.encode(payload, sec.deepInnerV).asJson,
+                     "path" -> "/realtime".asJson
+                   )
+                 )
+               )
+  } yield WebSocketFrame.text(text.noSpaces)
 
   protected def handleMessage(
     ws: WebSocket[RIO[AllEnv, *]],
@@ -38,6 +66,33 @@ private[liquid] trait WebSocketHandler { self: LiquidExchange.Service =>
     _    <- data.event match {
               case "pusher:connection_established"          =>
                 ws.send(subscribeText(subscribeChannel)) *> log.info("Connected!")
+              case "pusher_internal:subscription_succeeded" =>
+                log.debug("Subscribed ws!")
+              case `eventName`                              =>
+                ZIO.fromEither(decode[DataMessage](msg.payload)).flatMap(onEvent)
+              case a                                        => log.warn(s"Unexpected ws response: $a")
+            }
+  } yield ()).retryWhile(_.isInstanceOf[WebSocketClosed])
+
+  protected def handleAuthMessage(
+    ws: WebSocket[RIO[AllEnv, *]],
+    subscribeChannel: String,
+    eventName: String
+  )(
+    onEvent: DataMessage => ZIO[AllEnv, Throwable, Unit]
+  ): ZIO[AllEnv, Throwable, Unit] = (for {
+    msg  <- ws.receiveTextFrame()
+    _    <- log.trace(msg.payload)
+    data <- ZIO.fromEither(decode[WSMessage](msg.payload))
+    _    <- data.event match {
+              case "pusher:connection_established"          => log.info("Connected!") *>
+                  authText.flatMap(t => ws.send(t))
+              case "quoine:auth_success"                    => log.info("Authenticated!") *> ws.send(
+                  subscribeText(subscribeChannel)
+                )
+              case "quoine:auth_failure"                    => log.error("Auth failed!")
+              case "pusher_internal:subscription_succeeded" =>
+                log.debug("Subscribed ws!")
               case "pusher_internal:subscription_succeeded" =>
                 log.debug("Subscribed ws!")
               case `eventName`                              =>
