2a6c84b8084ecf3521518fffa64bad8ae4b7cd39
kazuki <pharcydetip@gmail.com>
2021-03-22 09:52:32 +0900

    feat: add mock data

--- frontend/pages/Prices.tsx
+++ frontend/pages/Prices.tsx
@@ -9,48 +9,10 @@ import {
 } from "recharts";
 
 const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
+  { time: "2020/1", coincheck: 1000, liquid: 2000 },
+  { time: "2020/2", coincheck: 1500, liquid: 1500 },
+  { time: "2020/3", coincheck: 1700, liquid: 1300 },
+  { time: "2020/4", coincheck: 1700, liquid: 2000 },
 ];
 
 const Prices = () => {
@@ -62,12 +24,12 @@ const Prices = () => {
       margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
     >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
+      <XAxis dataKey="time" />
       <YAxis />
       <Tooltip />
       <Legend />
-      <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      <Line type="monotone" dataKey="coincheck" stroke="#8884d8" />
+      <Line type="monotone" dataKey="liquid" stroke="#82ca9d" />
     </LineChart>
   );
 };
