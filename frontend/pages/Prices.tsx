fabf88a41b029951d01a07609072bde308012bd6
kazuki <pharcydetip@gmail.com>
2021-03-24 05:08:22 +0900

    feat: connect chart null portion

--- frontend/pages/Prices.tsx
+++ frontend/pages/Prices.tsx
@@ -9,10 +9,12 @@ import {
 } from "recharts";
 
 const data = [
-  { time: "2020/1", coincheck: 1000, liquid: 2000 },
-  { time: "2020/2", coincheck: 1500, liquid: 1500 },
-  { time: "2020/3", coincheck: 1700, liquid: 1300 },
+  { time: "2020/1", liquid: 2000 },
+  { time: "2020/2", coincheck: 1500 },
+  { time: "2020/3", liquid: 1300 },
+  { time: "2020/4", liquid: 2000 },
   { time: "2020/4", coincheck: 1700, liquid: 2000 },
+  { time: "2020/6", coincheck: 1500 },
 ];
 
 const Prices = () => {
@@ -28,8 +30,8 @@ const Prices = () => {
       <YAxis />
       <Tooltip />
       <Legend />
-      <Line type="monotone" dataKey="coincheck" stroke="#8884d8" />
-      <Line type="monotone" dataKey="liquid" stroke="#82ca9d" />
+      <Line type="monotone" dataKey="coincheck" stroke="#8884d8" connectNulls />
+      <Line type="monotone" dataKey="liquid" stroke="#82ca9d" connectNulls />
     </LineChart>
   );
 };
