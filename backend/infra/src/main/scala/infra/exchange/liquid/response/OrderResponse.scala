7997a4ba9bdd065fa4c0b19e72361d09673f9253
kazuki <pharcydetip@gmail.com>
2021-03-25 03:39:12 +0900

    feat: add TradeResponse

--- backend/infra/src/main/scala/infra/exchange/liquid/response/OrderResponse.scala
+++ backend/infra/src/main/scala/infra/exchange/liquid/response/OrderResponse.scala
@@ -1,7 +1,6 @@
 package infra.exchange.liquid.response
 
-import domain.exchange.liquid.LiquidOrder
-import domain.exchange.liquid.LiquidOrder.{Id, Price, Quantity, Status}
+import domain.exchange.liquid.{LiquidOrder, Trade}
 import lib.error.ClientDomainError
 import lib.refined.{PositiveDouble, PositiveLong}
 import zio.IO
@@ -12,8 +11,20 @@ final private[liquid] case class OrderResponse(
   quantity: PositiveDouble,
   status: String
 ) {
+  import domain.exchange.liquid.LiquidOrder.{Id, Price, Quantity, Status}
+
   def toOrder: IO[ClientDomainError, LiquidOrder] = Status
     .withNameZio(status).map(
       LiquidOrder(Id(id), Price(price), Quantity(price), _)
     )
+}
+
+final private[liquid] case class TradeResponse(
+  id: PositiveLong,
+  status: String
+) {
+  import domain.exchange.liquid.Trade._
+
+  def toTrade: IO[ClientDomainError, Trade] =
+    Status.withNameZio(status).map(Trade(Id(id), _))
 }
