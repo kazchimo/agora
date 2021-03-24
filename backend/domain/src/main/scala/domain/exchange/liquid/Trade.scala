8e2de8fa630177ef55ecae157fd46a3fe786cc6f
kazuki <pharcydetip@gmail.com>
2021-03-25 03:53:29 +0900

    feat: add closed method

--- backend/domain/src/main/scala/domain/exchange/liquid/Trade.scala
+++ backend/domain/src/main/scala/domain/exchange/liquid/Trade.scala
@@ -1,12 +1,15 @@
 package domain.exchange.liquid
 
+import domain.exchange.liquid.Trade.Status.Closed
 import domain.exchange.liquid.Trade._
 import domain.lib.{VOFactory, ZEnum}
 import enumeratum.EnumEntry.Snakecase
 import io.estatico.newtype.macros.newtype
 import lib.refined.PositiveLong
 
-final case class Trade(id: Id, status: Status)
+final case class Trade(id: Id, status: Status) {
+  def closed: Boolean = status == Closed
+}
 
 object Trade {
   @newtype case class Id(value: PositiveLong)
