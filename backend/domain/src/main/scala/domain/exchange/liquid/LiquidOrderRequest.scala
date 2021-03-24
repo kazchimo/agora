49901c5f397bef3c4d9ed2896234bb5f32cbaf9a
kazuki <pharcydetip@gmail.com>
2021-03-24 17:57:00 +0900

    feat: add leveraged request order

--- backend/domain/src/main/scala/domain/exchange/liquid/LiquidOrderRequest.scala
+++ backend/domain/src/main/scala/domain/exchange/liquid/LiquidOrderRequest.scala
@@ -1,8 +1,11 @@
 package domain.exchange.liquid
 
+import domain.exchange.liquid.FundingCurrency.Jpy
 import domain.exchange.liquid.LiquidOrder.OrderType.Limit
 import domain.exchange.liquid.LiquidOrder.Side.{Buy, Sell}
+import domain.exchange.liquid.LiquidOrder.TradingType.Cfd
 import domain.exchange.liquid.LiquidOrder._
+import eu.timepit.refined.auto._
 
 // https://developers.liquid.com/#create-an-order
 final case class LiquidOrderRequest[O <: OrderType, S <: Side] private (
@@ -10,7 +13,12 @@ final case class LiquidOrderRequest[O <: OrderType, S <: Side] private (
   productId: LiquidProduct.Id,
   side: S,
   quantity: Quantity,
-  price: Option[Price] = None
+  price: Option[Price] = None,
+  leverageLevel: Option[LeverageLevel] = None,
+  fundingCurrency: Option[FundingCurrency] = None,
+  tradingType: Option[TradingType] = None,
+  takeProfit: Option[TakeProfit] = None,
+  stopLoss: Option[StopLoss] = None
 )
 
 object LiquidOrderRequest {
@@ -27,4 +35,25 @@ object LiquidOrderRequest {
     price: LiquidOrder.Price
   ): LiquidOrderRequest[OrderType.Limit, Sell] =
     LiquidOrderRequest(Limit, productId, Sell, quantity, Some(price))
+
+  def leveraged[S <: Side](
+    productId: LiquidProduct.Id,
+    side: S,
+    quantity: Quantity,
+    price: Price,
+    leverageLevel: LeverageLevel = LeverageLevel.unsafeApply(2L),
+    takeProfit: TakeProfit,
+    stopLoss: StopLoss
+  ): LiquidOrderRequest[Limit.type, S] = LiquidOrderRequest(
+    Limit,
+    productId,
+    side,
+    quantity,
+    Some(price),
+    Some(leverageLevel),
+    Some(Jpy),
+    Some(Cfd),
+    Some(takeProfit),
+    Some(stopLoss)
+  )
 }
