a38152cbbccd4d1b3bb5349eb78d3583e29693b5
kazuki <pharcydetip@gmail.com>
2021-03-25 00:22:13 +0900

    feat: extends CirceEnum

--- backend/domain/src/main/scala/domain/exchange/liquid/FundingCurrency.scala
+++ backend/domain/src/main/scala/domain/exchange/liquid/FundingCurrency.scala
@@ -1,11 +1,13 @@
 package domain.exchange.liquid
 
 import domain.lib.ZEnum
+import enumeratum.CirceEnum
 import enumeratum.EnumEntry.Lowercase
 
 sealed trait FundingCurrency extends Lowercase
 
-object FundingCurrency extends ZEnum[FundingCurrency] {
+object FundingCurrency
+    extends ZEnum[FundingCurrency] with CirceEnum[FundingCurrency] {
   override def values: IndexedSeq[FundingCurrency] = findValues
 
   case object Jpy extends FundingCurrency
