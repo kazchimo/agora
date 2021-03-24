f057985cfbd742322ca67cbd55483b593e6a48e1
kazuki <pharcydetip@gmail.com>
2021-03-25 05:14:48 +0900

    feat: impl GetTrades

---
+++ backend/infra/src/main/scala/infra/exchange/liquid/impl/GetTrades.scala
@@ -0,0 +1,44 @@
+package infra.exchange.liquid.impl
+
+import domain.AllEnv
+import domain.exchange.liquid.{
+  FundingCurrency,
+  GetTradesParams,
+  LiquidExchange,
+  LiquidProduct,
+  Trade
+}
+import infra.exchange.liquid.Endpoints
+import infra.exchange.liquid.response.TradeResponse
+import zio.RIO
+import lib.syntax.all._
+import sttp.client3.UriContext
+import sttp.client3.circe.asJson
+import io.circe.generic.auto._
+import io.circe.refined._
+import cats.syntax.traverse._
+import zio.interop.catz._
+
+private[liquid] trait GetTrades extends AuthRequest {
+  self: LiquidExchange.Service =>
+  private def url(params: GetTradesParams) = {
+    val ps = Seq(
+      params.fundingCurrency.map(f => s"funding_currency=${f.entryName}"),
+      params.productId.map(p => s"product_id=${p.deepInnerV.toString}"),
+      params.side.map(s => s"side=${s.entryName}"),
+      params.status.map(s => s"status=${s.entryName}"),
+      params.tradingType.map(s => s"tradingType=${s.entryName}")
+    ).collect { case Some(s) => s }
+
+    "/trades" + (if (ps.isEmpty) "" else "?" + ps.mkString("&"))
+  }
+
+  override def getTrades(params: GetTradesParams): RIO[AllEnv, Seq[Trade]] =
+    for {
+      req    <- authRequest(url(params))
+      uri     = Endpoints.root + url(params)
+      res    <-
+        recover401Send(req.get(uri"$uri").response(asJson[Seq[TradeResponse]]))
+      trades <- res.map(_.toTrade).sequence
+    } yield trades
+}
