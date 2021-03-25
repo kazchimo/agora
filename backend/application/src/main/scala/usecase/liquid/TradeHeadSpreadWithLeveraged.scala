a789b4e69d3474ba02611b686cacbfadca8f6ae4
kazuki <pharcydetip@gmail.com>
2021-03-26 01:49:58 +0900

    change price

--- backend/application/src/main/scala/usecase/liquid/TradeHeadSpreadWithLeveraged.scala
+++ backend/application/src/main/scala/usecase/liquid/TradeHeadSpreadWithLeveraged.scala
@@ -2,9 +2,14 @@ package usecase.liquid
 
 import domain.broker.coincheck.liquid.LiquidBroker
 import domain.exchange.liquid.LiquidOrder.Side.{Buy, Sell}
-import domain.exchange.liquid.LiquidOrder.{Quantity, StopLoss, TakeProfit}
+import domain.exchange.liquid.LiquidOrder.{
+  LeverageLevel,
+  Quantity,
+  StopLoss,
+  TakeProfit
+}
 import domain.exchange.liquid.LiquidProduct.btcJpyId
-import domain.exchange.liquid.{LiquidExchange, LiquidOrderRequest}
+import domain.exchange.liquid.{LiquidExchange, LiquidOrderRequest, Trade}
 import eu.timepit.refined.auto._
 import eu.timepit.refined.numeric.Positive
 import lib.instance.all._
@@ -20,24 +25,29 @@ object TradeHeadSpreadWithLeveraged {
   val quantity: Quantity  = Quantity.unsafeFrom(0.001)
   val one: PositiveDouble = 1d
 
-  def trade = for {
+  def trade(tradeCount: PositiveInt) = for {
     latestBuyHeadPriceRef  <- LiquidBroker.latestHeadPriceRef(Buy)
     latestSellHeadPriceRef <- LiquidBroker.latestHeadPriceRef(Sell)
+    countRef               <- LiquidBroker.openTradeCountRef(Trade.Side.Long)
     requestOrder            = for {
-      buyPrice  <- latestBuyHeadPriceRef.get.someOrFailException
-      sellPrice <- latestSellHeadPriceRef.get.someOrFailException
-      plusOne   <- refineVZE[Positive, Double](buyPrice.deepInnerV + 1)
-      quote      = Ordering[PositiveDouble].max(sellPrice.value, plusOne)
-      stopLoss  <- StopLoss(buyPrice.value * 0.995)
-      request    = LiquidOrderRequest.leveraged(
-                     btcJpyId,
-                     Buy,
-                     quantity,
-                     buyPrice,
-                     TakeProfit(quote),
-                     stopLoss
-                   )
-      _         <- LiquidBroker.timeoutedOrder(request, 10.seconds)
+      c          <- countRef.get
+      _          <- log.debug(c.toString)
+      countIsOver = countRef.map(_ >= tradeCount.value)
+      _          <- (countIsOver.get <* ZIO.sleep(1.seconds))
+                      .repeatWhileEquals(true).whenM(countIsOver.get)
+      buyPrice   <- latestBuyHeadPriceRef.get.someOrFailException
+      quote      <- TakeProfit(buyPrice.deepInnerV * 1.0005)
+      stopLoss   <- StopLoss(buyPrice.value * 0.995)
+      request     = LiquidOrderRequest.leveraged(
+                      btcJpyId,
+                      Buy,
+                      quantity,
+                      buyPrice,
+                      quote,
+                      stopLoss,
+                      LeverageLevel.unsafeApply(2L)
+                    )
+      _          <- LiquidBroker.timeoutedOrder(request, 10.seconds)
     } yield ()
     _                      <- requestOrder
                                 .whenM(
